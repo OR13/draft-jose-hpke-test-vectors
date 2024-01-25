74bd16ccce213e62c362281e9b6af67fc56dcecd
Orie Steele <orie@transmute.industries>
2024-01-25 15:16:28 -0600

    :rocket:

--- src/json.ts
+++ src/json.ts
@@ -1,32 +1,38 @@
 import crypto from 'crypto';
 import { base64url } from "jose";
 
-import { publicKeyFromJwk, privateKeyFromJwk, HPKERecipient, isKeyAlgorithmSupported, suites, JOSE_HPKE_ALG, JWK, JWKS } from "./keys";
+import { publicKeyFromJwk, privateKeyFromJwk, HPKERecipient, isKeyAlgorithmSupported, suites, JOSE_HPKE_ALG, JWKS } from "./keys";
 
 import * as ContentEncryption from './ContentEncryption'
 
-export const encrypt = async (
-  enc: string,
-  plaintext: Uint8Array,
+export type RequestGeneralEncrypt = {
+  protectedHeader: { enc: 'A128GCM' }
+  plaintext: Uint8Array
+  additionalAuthenticatedData?: Uint8Array
   recipients: JWKS
+}
+
+export const encrypt = async (
+  req: RequestGeneralEncrypt
 ): Promise<any> => {
 
   // prepare the encrypted content for all recipients
+  let jwe = {} as any;
 
   // generate a content encryption key for a content encryption algorithm
-  const contentEncryptionKey = await ContentEncryption.generateContentEncryptionKey(enc);
+  const contentEncryptionKey = await ContentEncryption.generateContentEncryptionKey(req.protectedHeader.enc);
 
   // generate an initialization vector for use with the content encryption key
   const initializationVector = crypto.getRandomValues(new Uint8Array(12)); // possibly wrong
   const iv = base64url.encode(initializationVector)
-
+  jwe.iv = iv
   // create the protected header
   // top level protected header only has "enc"
-  const protectedHeader = base64url.encode(JSON.stringify({ enc }))
+  const protectedHeader = base64url.encode(JSON.stringify(req.protectedHeader))
 
   // encrypt the plaintext with the content encryption algorithm
-  const contentEncryptionAad = undefined
-  const ciphertext = base64url.encode(await ContentEncryption.encryptContent(enc, plaintext, initializationVector, contentEncryptionAad, contentEncryptionKey))
+  const ciphertext = base64url.encode(await ContentEncryption.encryptContent(req.protectedHeader.enc, req.plaintext, initializationVector, req.additionalAuthenticatedData, contentEncryptionKey))
+  jwe.ciphertext = ciphertext;
 
   // for each recipient public key, encrypt the content encryption key to the recipient public key
   // and add the result to the unprotected header recipients property
@@ -34,7 +40,7 @@ export const encrypt = async (
     recipients: [] as HPKERecipient[]
   }
 
-  for (const recipient of recipients.keys) {
+  for (const recipient of req.recipients.keys) {
     if (!isKeyAlgorithmSupported(recipient)) {
       throw new Error('Public key algorithm not supported.')
     }
@@ -64,54 +70,60 @@ export const encrypt = async (
       }
     )
   }
+  jwe.recipients = unprotectedHeader.recipients
 
-  // https://datatracker.ietf.org/doc/html/rfc7516#section-3.2
-  return {
-    protected: protectedHeader,
-    unprotected: unprotectedHeader,
-    iv,
-    ciphertext,
+  if (req.additionalAuthenticatedData){
+    jwe.aad = base64url.encode(req.additionalAuthenticatedData)
   }
+
+  jwe.protected = protectedHeader
+  return jwe;
 }
 
-export const decrypt = async (json: any, recipients: JWKS): Promise<any> => {
-  const { protected: protectedHeader, unprotected, iv, ciphertext } = json;
+export type RequestGeneralDecrypt = {
+  jwe: any,
+  privateKeys: JWKS
+}
+
+
+export const decrypt = async (req: RequestGeneralDecrypt): Promise<any> => {
+  const { protected: protectedHeader, recipients, iv, ciphertext, aad } = req.jwe;
 
   // find a recipient for which we have a private key
   let matchingRecipient = undefined
   let matchingPrivateKey = undefined
-  for (const privateKey of recipients.keys){
-    const recipient = unprotected.recipients.find((r: HPKERecipient)=>{
+  for (const privateKey of req.privateKeys.keys) {
+    const recipient = recipients.find((r: HPKERecipient) => {
       return r.kid === privateKey.kid
     })
-    if (recipient){
+    if (recipient) {
       // we have a private key for this recipient
       matchingRecipient = recipient;
       matchingPrivateKey = privateKey;
       break
     }
-  } 
+  }
 
-  if (!matchingRecipient || !matchingPrivateKey){
+  if (!matchingRecipient || !matchingPrivateKey) {
     throw new Error('No decryption key found for the given recipients')
   }
 
-  if (!isKeyAlgorithmSupported(matchingPrivateKey)){
+  if (!isKeyAlgorithmSupported(matchingPrivateKey)) {
     throw new Error('Private key algorithm not supported.')
   }
   // We could check here to see if the "enc" in the protected header
   // matches the last part of the "alg" on the private key.
-  
+
 
   const suite = suites[matchingPrivateKey.alg as JOSE_HPKE_ALG]
-  
+
   // TODO: mixed recipients support goes here... (ECDH-ES and HPKE)
 
   // selected the encapsulated_key for the recipient
   const { encapsulated_key, encrypted_key } = matchingRecipient;
 
   // create the HPKE recipient
-   const recipient = await suite.createRecipientContext({
+  const recipient = await suite.createRecipientContext({
     recipientKey: await privateKeyFromJwk(matchingPrivateKey),
     enc: base64url.decode(encapsulated_key)
   })
@@ -119,21 +131,27 @@ export const decrypt = async (json: any, recipients: JWKS): Promise<any> => {
   // compute the additional data from the protected header
   // this will be used to protect against 
   // a cross mode attack on the aead
-  const additionalData = new TextEncoder().encode(protectedHeader)
+  const hpkeOpenAad = new TextEncoder().encode(protectedHeader)
 
   // open the content encryption key for the given content encryption algorithm
   // which is described in the protected header
-  const decryptedContentEncryptionKey = await recipient.open(base64url.decode(encrypted_key), additionalData)
+  const decryptedContentEncryptionKey = await recipient.open(base64url.decode(encrypted_key), hpkeOpenAad)
 
   // determine the content encryption algorithm
   // now that we know we have a key that supports it
-  const { enc } = JSON.parse(new TextDecoder().decode(base64url.decode(protectedHeader)));
+  const parsedProtectedHeader = JSON.parse(new TextDecoder().decode(base64url.decode(protectedHeader)));
 
   // decrypt the ciphertext
   const ct = base64url.decode(ciphertext)
   const initializationVector = base64url.decode(iv);
   const contentEncryptionKey = new Uint8Array(decryptedContentEncryptionKey)
-  const contentEncryptionAad = undefined;
-  const plaintext = await ContentEncryption.decryptContent(enc, ct, initializationVector, contentEncryptionAad, contentEncryptionKey)
-  return new Uint8Array(plaintext)
+  let contentEncryptionAad = undefined;
+  if (aad){
+    contentEncryptionAad = base64url.decode(aad)
+  }
+  const plaintext = await ContentEncryption.decryptContent(parsedProtectedHeader.enc, ct, initializationVector, contentEncryptionAad, contentEncryptionKey)
+  const decryption = { plaintext: new Uint8Array(plaintext)} as any;
+  decryption.protectedHeader = parsedProtectedHeader;
+  decryption.aad = contentEncryptionAad;
+  return decryption
 }
--- tests/json.test.ts
+++ tests/json.test.ts
@@ -28,21 +28,44 @@ it('encrypt / decrypt', async () => {
     ]
   }
 
-  const message = `It’s a 💀 dangerous business 💀, Frodo, going out your door.`
-  const plaintext = new TextEncoder().encode(message);
+  const plaintext = new TextEncoder().encode(`It’s a 💀 dangerous business 💀, Frodo, going out your door.`);
+  const aad = new TextEncoder().encode('💀 aad')
   const contentEncryptionAlgorithm = 'A128GCM'
 
-  const ciphertext = await hpke.json.encrypt(contentEncryptionAlgorithm, plaintext, recipientPublicKeys)
+  const ciphertext = await hpke.json.encrypt({
+    protectedHeader: { enc: contentEncryptionAlgorithm },
+    plaintext,
+    additionalAuthenticatedData: aad,
+    recipients: recipientPublicKeys
+  });
 
   // console.log(JSON.stringify(ciphertext, null, 2))
+  // {
+  //   "iv": "jGA1495md8IaMrbO",
+  //   "ciphertext": "L7sD6Y2RfnlElDF_la5eB...jGeNoaZZFp_F6yZ3K-uqg",
+  //   "recipients": [
+  //     {
+  //       "kid": "urn:ietf:params:oauth:jwk-thumbprint:sha-256:ROv0KdmDUQBERQuiTnp5S-Ki4rgjqPtTX_w1dTfPsWg",
+  //       "encapsulated_key": "BIv2gwvAGZzp1...B36hqdTcZh41jH7LlCChU",
+  //       "encrypted_key": "CI4_LXJdHLWsvOA6SQGMSH3...uDpdrw5Fn0Po5Ho9"
+  //     },
+  //     {
+  //       "kid": "urn:ietf:params:oauth:jwk-thumbprint:sha-256:C4LqgFbEPz97P9wRoS1DyBk4FT7CxOuPNn9a8gyoxOU",
+  //       "encapsulated_key": "BK_M7wMl1NQPy_UGGxOb6...lvppxiDCnCEqPT4isU",
+  //       "encrypted_key": "MuTYjX0g1emKI481spI...maP_T9RfsZCmSDCqS5"
+  //     }
+  //   ],
+  //   "aad": "8J-SgCBhYWQ",
+  //   "protected": "eyJlbmMiOiJBMTI4R0NNIn0"
+  // }
 
   for (const recipient of recipientPublicKeys.keys){
     const privateKey = resolvePrivateKey(recipient.kid)
     // simulate having only one of the recipient private keys
     const recipientPrivateKeys =  { "keys": [ privateKey ] }
-
-    const recovered = await hpke.json.decrypt(ciphertext, recipientPrivateKeys)
-    expect(new TextDecoder().decode(recovered)).toBe(message);
+    const decryption = await hpke.json.decrypt({ jwe: ciphertext, privateKeys: recipientPrivateKeys})
+    expect(new TextDecoder().decode(decryption.plaintext)).toBe(`It’s a 💀 dangerous business 💀, Frodo, going out your door.`);
+    expect(new TextDecoder().decode(decryption.aad)).toBe('💀 aad');
   }
   
 })
