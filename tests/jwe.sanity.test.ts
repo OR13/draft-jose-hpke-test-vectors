288dc19c56f7e21d99a37b04ebf730e89412c030
Orie Steele <orie@transmute.industries>
2024-02-11 15:30:14 -0600

    :rocket:

--- tests/jwe.sanity.test.ts
+++ tests/jwe.sanity.test.ts
@@ -3,7 +3,7 @@ import * as jose from 'jose'
 
 import * as mixed from '../src/mixed'
 
-it('jwe json', async () => {
+it('jwe json multiple recipient', async () => {
   const key1 = await jose.generateKeyPair('ECDH-ES+A128KW', { crv: 'P-256', extractable: true })
   const key2 = await jose.generateKeyPair('RSA-OAEP-384')
   const message = new TextEncoder().encode('✨ It’s a dangerous business, Frodo, going out your door. ✨')
@@ -34,7 +34,28 @@ it('jwe json', async () => {
   const cek = mixed.unwrap('A128KW', sharedSecret, encryptedKey)
   const kwkc = mixed.wrap('A128KW', sharedSecret, cek)
   expect(encryptedKey).toEqual(kwkc)
+})
 
+
+it('jwe json single recipient', async () => {
+  const key1 = await jose.generateKeyPair('ECDH-ES+A128KW', { crv: 'P-256', extractable: true })
+  const message = new TextEncoder().encode('✨ It’s a dangerous business, Frodo, going out your door. ✨')
+  const aad = new TextEncoder().encode('💀 aad')
+  const jwe = await new jose.GeneralEncrypt(
+    message
+  )
+    .setAdditionalAuthenticatedData(aad)
+    .setProtectedHeader({ enc: 'A128GCM' })
+    .addRecipient(key1.publicKey)
+    .setUnprotectedHeader({ alg: 'ECDH-ES+A128KW' })
+    .encrypt()
+  const { plaintext, protectedHeader, additionalAuthenticatedData } = await jose.generalDecrypt(jwe, key1.privateKey) as any;
+  expect(new TextDecoder().decode(additionalAuthenticatedData)).toBe('💀 aad')
+  expect(new TextDecoder().decode(plaintext)).toBe('✨ It’s a dangerous business, Frodo, going out your door. ✨')
+  expect(protectedHeader.enc).toBe('A128GCM')
+  expect(protectedHeader.epk.kty).toBe('EC')
+  expect(protectedHeader.epk.crv).toBe('P-256')
+  
 })
 
 it('jwe compact', async () => {
@@ -50,6 +71,6 @@ it('jwe compact', async () => {
   expect(protectedHeader.alg).toBe('ECDH-ES+A128KW')
   expect(protectedHeader.enc).toBe('A128GCM')
   // protected header also protectes the epk.
-  
+
   expect(new TextDecoder().decode(plaintext)).toBe('It’s a dangerous business, Frodo, going out your door.')
 })
