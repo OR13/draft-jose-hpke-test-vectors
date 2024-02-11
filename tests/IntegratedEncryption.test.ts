a4fd321a8bf54ac9d907dea301916326214bde86
Orie Steele <orie@transmute.industries>
2024-02-11 15:53:06 -0600

    :rocket:

--- tests/IntegratedEncryption.test.ts
+++ tests/IntegratedEncryption.test.ts
@@ -12,6 +12,7 @@ describe('encrypt / decrypt ', ()=>{
     expect(jwe.split('.').length).toBe(5) // compact jwe is default
     const recovered = await hpke.IntegratedEncryption.decrypt(jwe, privateKeyJwk)
     expect(new TextDecoder().decode(recovered)).toBe(message);
+    console.log(jwe)
   })
   it('JSON', async () => {
     const privateKeyJwk = await hpke.keys.generate('HPKE-Base-P256-SHA256-AES128GCM')
@@ -19,6 +20,7 @@ describe('encrypt / decrypt ', ()=>{
     const message = `It’s a 💀 dangerous business 💀, Frodo, going out your door.`
     const plaintext = new TextEncoder().encode(message);
     const jwe = await hpke.IntegratedEncryption.encrypt(plaintext, publicKeyJwk, { serialization: 'GeneralJson'})
+    console.log(jwe)
     expect(jwe.protected).toBeDefined()
     expect(jwe.ciphertext).toBeDefined()
     expect(jwe.iv).toBeUndefined()
